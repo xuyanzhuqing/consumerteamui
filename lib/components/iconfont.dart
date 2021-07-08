b3efc704eb0de55c35af924a5d0f921c0b893e0b
徐存根 <18392352270@163.com>
2021-07-08 16:22:07 +0800

    新增规划图片，规划iconfont

---
+++ lib/components/iconfont.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class Iconfont {
+  Iconfont._();
+
+  static const String name = 'iconfont';
+
+  static const IconData heart = IconData(
+    0xe8ab,
+    fontFamily: Iconfont.name,
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:consumerteamui/components/iconfont.dart';
 
 void main() {
   runApp(MyApp());
@@ -93,6 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Icon(Iconfont.heart),
             Text(
               'You have pushed the button this many times:',
             ),
