22066b971e5236d4eaea8ec1f97bd215b199db4f
yang.xu <yang.xu@fuchuang-auto.com>
2021-08-22 15:27:24 +0800

    feat: 添加iconfonts

---
+++ lib/iconBuild/fonts/autoicons.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Autoicons {
+  static const String _family = 'autoicons';
+  Autoicons._();
+  
+      static const IconData back =
+      IconData(0xea01, fontFamily: _family);
+      static const IconData fd =
+      IconData(0xea02, fontFamily: _family);
+      static const IconData ld =
+      IconData(0xea03, fontFamily: _family);
+
+}
--- lib/team/views/home.dart
+++ lib/team/views/home.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:consumerteamui/iconBuild/fonts/autoicons.dart';
 
 class TeamHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('team home')
+        title: Icon(Autoicons.back)
       ),
     );
   }
